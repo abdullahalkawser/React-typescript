a090a9a5f6a94d0ded4e39295c4a0fdb22f3c031
abdullahalkawser <abdullahalkawser1@gmail.com>
2024-09-26 01:30:58 +0600

    user defined Props types array,object

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,12 +4,22 @@ import User from './componants/user'
 
 function App() {
 
+  const user ={
+    name:'raniya',
+    age: 34,
+    lang:["bangla","hindi"],
+    register:true
+  }
+
 
   return (
-<div>
+<div className=' '>
   <h1 className='text-5xl mb-10'>React -Typescriyt</h1>
-  <User name='abdullah al kawser' age= {39} register= {false}/>
-  <User name='hassan' age= {390} register= {true}/>
+ <div className='flex '>
+ {/* <User name='abdullah al kawser' age= {39} register= {false} language={['bangla' ,'Arabic']}/>
+ <User name='hassan' age= {390} register= {true} language={['english' ,'Arabic']}/> */}
+ <User user={user} />
+ </div>
 </div>
   )
 }
--- my-app/src/componants/user.tsx
+++ my-app/src/componants/user.tsx
@@ -1,19 +1,63 @@
 
+// type userPropos={
+//     name:string,
+//     age:number,
+//     register:boolean,
+//     language:string[]
+// }
+
+// objcet definne
 type userPropos={
-    name:string,
-    age:number,
-    register:boolean
+  user: {
+    name: string;
+    age: number;
+    register:boolean;
+    lang: string[];
+  }
 }
+
+
+// //{ name: string; age: number}
+// const User = ({user} :userPropos) => {
+//   const { name,age,lang}= user;
+//   return (
+//     <div>
+//         <h1>users </h1>
+//         <h3>{props.name}</h3>
+//         <p>{props.age}</p>
+//         {props.register ? <p>user is regiterd</p>:  <p>user is not regiterd</p>}
+
+//    <p>Speksr:
+
+//     {
+//       props.language.map((lang,index)=>(
+//        <p key={index}>{lang}</p>
+//       ))
+//     }
+//    </p>
+//     </div>
+//   )
+// }
+
+
 //{ name: string; age: number}
-const User = (props :userPropos) => {
+const User = ({user} :userPropos) => {
+  const { name,age,lang,register}= user;
   return (
     <div>
         <h1>users </h1>
-        <h3>{props.name}</h3>
-        <p>{props.age}</p>
-        {props.register ? <p>user is regiterd</p>:  <p>user is not regiterd</p>}
+        <h3>{name}</h3>
+        <p>{age}</p>
+        {register ? <p>user is regiterd</p>:  <p>user is not regiterd</p>}
+
+   <p>Speksr:
+    {
+      lang.map((lang,index)=>(
+       <p key={index}>{lang}</p>
+      ))
+    }
+   </p>
     </div>
   )
 }
-
 export default User
